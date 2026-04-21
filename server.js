6f3579503d22a3052087f6db83202d4b0832e532
Adnaya-BridgeStudio <adnayadigital@gmail.com>
2026-04-21 12:38:15 +0300

    server.js

--- server.js
+++ server.js
@@ -22,6 +22,10 @@ process.env.GOOGLE_CLIENT_SECRET,
 process.env.GOOGLE_REDIRECT_URI
 );
 
+oauth2Client.setCredentials({
+refresh_token:
+process.env.GOOGLE_REFRESH_TOKEN
+});
 
 // =======================
 // TOKEN
